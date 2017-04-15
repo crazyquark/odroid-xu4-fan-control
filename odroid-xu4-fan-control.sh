0da9a18135ddbb9d978b0eb73566d824b0fa8aae
Cristian Sandu <cristian.sandu@gmail.com>
2017-04-15 04:29:33 +0300

    fix fan path

--- odroid-xu4-fan-control.sh
+++ odroid-xu4-fan-control.sh
@@ -17,8 +17,8 @@ fi
 
 
 TEMPERATURE_FILE="/sys/devices/10060000.tmu/temp"
-FAN_MODE_FILE="/sys/devices/odroid_fan.13/fan_mode"
-FAN_SPEED_FILE="/sys/devices/odroid_fan.13/pwm_duty"
+FAN_MODE_FILE="/sys/devices/odroid_fan.14/fan_mode"
+FAN_SPEED_FILE="/sys/devices/odroid_fan.14/pwm_duty"
 TEST_EVERY=3 #seconds
 new_fan_speed_default=80
 LOGGER_NAME=odroid-xu3-fan-control
